79d9f01746dd248af710af642361c6a6784e996a
Ben Dod <benjdod@live.unc.edu>
2021-05-04 11:35:47 -0400

    compiler has options now

--- src/miniJava/Compiler.java
+++ src/miniJava/Compiler.java
@@ -1,7 +1,6 @@
 package miniJava;
 
 import java.io.FileReader;
-import java.nio.charset.StandardCharsets;
 
 import miniJava.ContextualAnalysis.ContextualAnalyzer;
 import miniJava.SyntacticAnalyzer.Parser;
@@ -9,10 +8,7 @@ import miniJava.SyntacticAnalyzer.Scanner;
 import miniJava.AbstractSyntaxTrees.*;
 import miniJava.AbstractSyntaxTrees.Package;
 import miniJava.CodeGenerator.CodeGenerator;
-import mJAM.Interpreter;
 import mJAM.ObjectFile;
-import mJAM.Disassembler;
-
 public class Compiler {
 	
 	static final int PARSE_SUCCESS = 0;
@@ -22,7 +18,9 @@ public class Compiler {
 
 	static ErrorReporter _reporter = new ErrorReporter();
 
-	// a comment
+	static boolean verbose = false;
+	static boolean quiet = false;
+	static boolean color = false;
 	
 	static void exitProgram(int exitcode) {
 		// System.out.println("Exiting with code " + exitcode);
@@ -30,28 +28,50 @@ public class Compiler {
 	}
 
 	static void printGreen(String s) {
-		System.out.println("\u001B[0;92m" + s + "\u001B[0m");
+		if (! quiet) System.out.println((color ? "\u001B[0;92m" : "") + s + (color ? "\u001B[0m" : ""));
+	}
+
+	static void printRed(String s) {
+		if (! quiet) System.out.println((color ? "\u001B[0;91m" : "") + s + (color ? "\u001B[0m" : ""));
+	}
+
+	static void printVerboseGreen(String s) {
+		if (verbose) printGreen(s);
+	}
+
+	static void printVerboseRed(String s) {
+		if (verbose) printRed(s);
+	}
+
+	static void printVerbose(String s) {
+		if (verbose) System.out.println(s);
 	}
 
 	static void checkForErrors() {
-		if (_reporter.hasErrors()) {
-			//System.out.println("\u001B[0;91m----- Compilation failed! -----\u001B[0m");
-			System.out.print("\u001B[0;91mfailed\u001B[0m\n");
-			_reporter.prinsOutput();
-			//System.out.print("\u001B[0;91m          ----------\u001B[0m");
-			exitProgram(PARSE_FAILURE);
-		}
+		if (! quiet)
+			if (_reporter.hasErrors()) {
+				printRed("failed");
+				_reporter.prinsOutput();
+				exitProgram(PARSE_FAILURE);
+			}
 	}
 
 	static void checkForError() {
-		if (_reporter.hasErrors()) {
-			System.out.print("failed ---\n");
-			_reporter.prinsOutput(_reporter.getFirst());
-			exitProgram(PARSE_FAILURE);
-		}
+		if (! quiet)
+			if (_reporter.hasErrors()) {
+				System.out.print("failed ---\n");
+				_reporter.prinsOutput(_reporter.getFirst());
+				exitProgram(PARSE_FAILURE);
+			}
 	}
 
-	static void runCompiler(String filepath) {
+	/**
+	 * Compiles a miniJava program
+	 * @param filepath path to the miniJava source code
+	 * @return path to the resulting object file
+	 */
+
+	public static String compile(String filepath) {
 				
 		int l = filepath.lastIndexOf('.');
 		
@@ -81,40 +101,21 @@ public class Compiler {
 		Scanner s = new Scanner(f, _reporter);
 		Parser p = new Parser(s, _reporter);
 
-		System.out.println("--- parsing ---");
+		//printVerbose("--- parsing ---");
 		Package tree = p.parse();
 
 		checkForError();
-		printGreen("--- completed ---");
+		printVerbose("AST generation complete.");
 
-		
-		
-		ASTDisplay adt = new ASTDisplay();
-		adt.showTree(tree);  
-		
-
-		//System.out.println("--- identification ---");
-		//Identification idn = new Identification(tree, _reporter);
-
-		//checkForError();
-
-		//System.out.println("--- type checking ---");
-		//TypeChecker tc = new TypeChecker(tree, _reporter);
-
-
-		System.out.println("--- contextual analysis ---");
 		ContextualAnalyzer ca = new ContextualAnalyzer(tree, _reporter);
 
 		checkForErrors();
-		printGreen("--- completed ---");
-
+		printVerbose("contextual analysis complete.");
 		
-		
-		System.out.println("--- code generation ---");
 		CodeGenerator cg = new CodeGenerator(tree, _reporter);	
 		
 		checkForErrors();
-		printGreen("--- completed ---");
+		printVerbose("code generation complete.");
 		
 		String object_filepath = filepath.substring(0, filepath.lastIndexOf(".")) + ".mJAM";
 		
@@ -124,33 +125,9 @@ public class Compiler {
 		if (failed) {
 			System.out.println("Error: could not write object file!");
 			exitProgram(FILE_FAILURE);
-		} else {
-			//System.out.println("Wrote object file to " + object_filepath);
-		}
-		
+		} 
 
-		
-		
-		
-		Disassembler d = new Disassembler(object_filepath);
-		d.disassemble();
-		
-		boolean debug = true;
-		
-		if (debug) {
-			Interpreter.debug(object_filepath, filepath);
-		} else {
-			Interpreter.interpret(object_filepath);
-		}
-		
-
-		System.out.println("\u001B[0;32mCompilation successful.\u001B[0m");
-		
-
-		/*
-		ASTDisplay adt = new ASTDisplay();
-		adt.showTree(tree); 
-		*/
+		printVerbose("object file written.");
 
 		try {
 			if (f != null)
@@ -159,12 +136,55 @@ public class Compiler {
 			System.err.println(e);
 			System.exit(COMMAND_FAILURE);
 		}
+
+		printVerboseGreen("Compilation successful.");
+
+		return object_filepath;
 		
-		System.exit(PARSE_SUCCESS);
+		// System.exit(PARSE_SUCCESS);
 	}
+
+	public static String compile(String[] args) {
 	
-	public static void compile(String filepath) {
-		runCompiler(filepath);
+		return compile(parseArgs(args));
+	}
+
+	private static void argFail(String msg, String optStr) {
+		printRed(msg + " '" + optStr + "'");
+		System.exit(FILE_FAILURE);
+	}
+
+	private static String parseArgs(String[] args) {
+
+		String target = null;
+
+		for (String arg : args) {
+
+			if (arg.charAt(0) == '-') {
+
+				if (arg.length() <= 1) argFail("bad option", arg);
+
+				String a = arg.substring(1);
+
+				if (a.contentEquals("q") || a.contentEquals("-quiet")) {
+					quiet = true;
+				}  else if (a.contentEquals("v") || a.contentEquals("-verbose")) {
+					verbose = true;
+					quiet = false;
+				} else if (a.contentEquals("c") || a.contentEquals("-color")) {
+					color = true;
+				}
+			} 
+
+			else {
+				if (target == null) target = arg;
+				else argFail("duplicate filepath" ,arg);
+			}
+		}
+
+		if (target == null ) argFail("no filepath specified", "");
+
+		return target;
 	}
 	
 	public static void main(String[] args) {
@@ -175,19 +195,23 @@ public class Compiler {
 			exitProgram(COMMAND_FAILURE);
 		} */
 		
+		parseArgs(args);
 		
-		String target;
+		String target = "";
 		
 		if (args.length < 1) {
-			//target = "../tests/pa4_tests/pass401.java";
-			//target = "./test/features/string/pass106.java";
-			target = "./test/test.java";
+			System.out.println("must provide a path to a miniJava program");
+			System.exit(FILE_FAILURE);
 		} else {
 			target = args[0];
 		}
+
 		
 		
-		runCompiler(target);
+		
+		compile(target);
+		printVerboseGreen("Compilation successful");
+		System.exit(PARSE_SUCCESS);
 	}
 
 }
---
+++ src/miniJava/Harness.java
@@ -0,0 +1,25 @@
+package miniJava;
+
+import mJAM.Disassembler;
+import mJAM.Interpreter;
+
+public class Harness {
+    public static void main(String[] args) {
+
+        String filepath = "./test/test.java";
+
+
+        String object_filepath = Compiler.compile(new String[] {"-v", "-c", filepath});
+
+        Disassembler d = new Disassembler(object_filepath);
+        d.disassemble();
+		
+        boolean debug = true;
+		
+        if (debug) {
+            Interpreter.debug(object_filepath, filepath);
+        } else {
+            Interpreter.interpret(object_filepath);
+        }
+    }
+}
